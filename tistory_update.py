bb5ed6758cd181d36f43e5e7fd8e8e4f252789a1
JH <46816743+JiHongKim15@users.noreply.github.com>
2021-10-07 10:36:22 +0900

    Create tistory_update.py

---
+++ tistory_update.py
@@ -0,0 +1,23 @@
+import feedparser
+
+
+blog_rss_url = "https://honeywater97.tistory.com/rss"
+rss_feed = feedparser.parse(blog_rss_url)
+
+MAX_POST_NUM = 10
+
+latest_blog_post_list = ""
+
+MAX_POST_NUM = 10
+
+
+for idx, feed in enumerate(rss_feed['entries']):
+    if idx > MAX_POST_NUM:
+        break
+    feed_date = feed['published_parsed'] 
+    latest_blog_post_list += f"[{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
+
+markdown_text = """https://honeywater97.tistory.com 최신 목록"""
+readme_text = f"{markdown_text}{latest_blog_post_list}"
+with open("README.md", 'w', encoding='utf-8') as f:
+    f.write(readme_text)
